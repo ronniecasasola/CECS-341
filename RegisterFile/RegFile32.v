70341671a3d821d9cb090a291c4702cb8fc81de4
Ronnie Casasola <ronald.casasola@student.csulb.edu>
2017-07-12 00:22:28 -0700

    Add files via upload

---
+++ RegisterFile/RegFile32.v
@@ -0,0 +1,24 @@
+`timescale 1ns / 1ps
+
+module RegFile32(Read1, Read2, WriteReg,WriteData, clock, RegWrite, Data1, Data2);
+
+	input [4:0] Read1, Read2, WriteReg; //the register numbers to read or write
+	input [31:0] WriteData; //Data to write
+	input RegWrite, //the write control signal
+			clock; //the clock to trigger a write
+			
+	output [31:0] Data1, Data2; //the register values to be read
+	reg [31:0] RF [31:0]; //32 registers each 32 bits wide
+	
+	assign Data1 = RF[Read1];
+	assign Data2 = RF[Read2];
+	
+	always @(negedge clock)
+	begin
+		//if RegWrite control signal is asserted
+		if (RegWrite == 1 && WriteReg != 5'h000) RF[WriteReg] <= WriteData;
+		
+		RF[0] <= 32'h00000000; //ensure $0 == 0
+	end
+endmodule
+
---
+++ RegisterFile/RegFileTester.v
@@ -0,0 +1,72 @@
+`timescale 1ns / 1ps
+
+module RegFile32_Tester;
+
+	// Inputs
+	reg [4:0] Read1;
+	reg [4:0] Read2;
+	reg [4:0] WriteReg;
+	reg [31:0] WriteData;
+	reg clock;
+	reg RegWrite;
+
+	// Outputs
+	wire [31:0] Data1;
+	wire [31:0] Data2;
+
+	// Instantiate the Unit Under Test (UUT)
+	RegFile32 uut (
+		.Read1(Read1), 
+		.Read2(Read2), 
+		.WriteReg(WriteReg), 
+		.WriteData(WriteData), 
+		.clock(clock), 
+		.RegWrite(RegWrite), 
+		.Data1(Data1), 
+		.Data2(Data2)
+	);
+
+	integer i; // loop index variable
+	
+	always #5 clock = ~clock; 
+
+	initial begin
+		// Initialize Inputs
+		Read1 = 0;
+		Read2 = 0;
+		WriteReg = 0;
+		WriteData = 0;
+		clock = 0;
+		RegWrite = 1;
+		#10;
+        
+		  //when RegWrite is set to 1, then we write into the registers
+		$display ("Writing to registers");
+		$display ("------------------------------------");
+		
+		for (i = 0; i < 32; i = i + 1)
+		begin
+			@(posedge clock)
+			begin
+				WriteData = $random;
+				WriteReg = i;
+			end
+			@(negedge clock)
+				#0 $display ("$%d <-0x%h", WriteReg, WriteData);
+		end
+		
+		#5 RegWrite = 0;
+		
+		$display ("Reading data from the registers");
+		$display ("------------------------------------");
+		
+		for (i = 0; i < 32; i = i +2)
+		begin
+			Read1 = i;
+			Read2 = i+1;
+			#0 $display("$%d = 0x%h", Read1, Data1);
+			#0 $display("$%d = 0x%h", Read2, Data2);
+		end
+		$stop;
+		end
+endmodule
